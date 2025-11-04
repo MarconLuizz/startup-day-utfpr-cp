97dfde9ec83092270e131f011813a79400f75d39
visiondigi <mjl.inovasimples@gmail.com>
2025-11-04 19:47:59 -0300

    Textos do FAQ

--- src/components/sections/FAQ/faqData.js
+++ src/components/sections/FAQ/faqData.js
@@ -2,7 +2,7 @@ export const faq = [
   {
     id: 1,
     q: "Precisa me inscrever antes?",
-    a: "Não! A inscrição é feita no dia do evento, às 13h. Basta chegar no horário!",
+    a: "Não! A inscrição é feita no dia do evento, às 13h. Serão disponibilizados um credenciamento para os visitantes.",
   },
   {
     id: 2,
@@ -12,11 +12,11 @@ export const faq = [
   {
     id: 3,
     q: "O que é a sessão de 'Conte sua ideia'?",
-    a: "É a sua oportunidade de apresentar sua ideia de negócio em poucos minutos para uma banca de especialistas e para o público, recebendo feedbacks e concorrendo a uma premiação simbólica.",
+    a: "É a sua oportunidade de apresentar sua ideia de negócio em poucos minutos para uma banca de especialistas e para o público, onde os 3 mais votados receberão prêmios!",
   },
   {
     id: 4,
     q: "Haverá certificado de participação?",
-    a: "Sim — certificação digital para participantes do evento.",
+    a: "Sim — certificação digital para participantes do evento que participarem de até 2 dinâmicas, onde serão disponibilizados QRCodes para confirmar a participação.",
   },
 ];
